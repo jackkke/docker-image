62f78b5c34a319619921a501b5518a1e65b3218a
jackkke <32036121+jackkke@users.noreply.github.com>
2026-05-06 12:25:07 +0800

    gradle 支持自定义参数 以及追加参数

--- pangu-gradle/entrypoint.sh
+++ pangu-gradle/entrypoint.sh
@@ -5,20 +5,11 @@ case "$TASK" in
   war) GRADLE_TASK=":pangu:pangu-webres:war" ;;
   oem) GRADLE_TASK="assemblePainting" ;;
   ddl) GRADLE_TASK=":pangu:compileDDL" ;;
-  *) GRADLE_TASK="$TASK" ;;
+  *) exec gradle "$@";;
 esac
-# -----------------------------
-# 拼接所有 Gradle 参数
-# -----------------------------
-GRADLE_ARGS=""
-CLEAN_FLAG=$(echo "${CLEAN:-true}" | tr '[:upper:]' '[:lower:]')
-[ "$CLEAN_FLAG" != "false" ] && [ "$CLEAN_FLAG" != "0" ] && GRADLE_ARGS="$GRADLE_ARGS clean"
-GRADLE_ARGS="$GRADLE_ARGS $GRADLE_TASK"
-# Artifactory
-[ -n "$ARTIFACTORY_HOST" ] && GRADLE_ARGS="$GRADLE_ARGS -Partifactory_host=$ARTIFACTORY_HOST"
-# daemon 控制（默认关闭 daemon）
-[ "$(echo "${DAEMON:-false}" | tr '[:upper:]' '[:lower:]')" != "true" ] && GRADLE_ARGS="$GRADLE_ARGS --no-daemon"
-# -----------------------------
-# 执行
-# -----------------------------
-exec gradle $GRADLE_ARGS
+shift 1
+ARGS="$GRADLE_TASK"
+[ "${CLEAN:-false}" = "true" ] && ARGS="clean $ARGS"
+[ -n "$ARTIFACTORY_HOST" ] && ARGS="$ARGS -Partifactory_host=$ARTIFACTORY_HOST"
+[ "${DAEMON:-true}" != "true" ] && ARGS="$ARGS --no-daemon"
+exec gradle $ARGS "$@"
